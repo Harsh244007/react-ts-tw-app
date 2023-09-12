e261d7a89ca01d75b55bfb4eafe48108d3ad6efa
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-13 00:44:56 +0530

    * Initial commit .

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App=()=> {
   return (
     <>
   <p className="text-blue text-center text-2xl">Hello There</p>
+  <p className="text-blue text-center text-2xl">This is an template please add or remove files here.</p>
     </>
   )
 }
